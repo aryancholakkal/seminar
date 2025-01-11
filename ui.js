ae2ef033bd545ffe3b33e6116444c7b5ac0d40c9
aryancholakkal <aryancholakkal2002@gmail.com>
2025-01-11 11:56:06 +0530

    finished echo function

--- ui.js
+++ ui.js
@@ -1,3 +1,8 @@
-function changeColor() {
-  document.getElementById("body").style.color = "red";
+function echo() {
+
+  // get the value from input field
+  const name = document.getElementById("name").value;
+
+  // place the greeting in p field
+  document.getElementById("greeting").innerHTML = `Hi there ${name}!`;
 }
